7886c63066b7b2985d5915ae019329a453738565
Adam Shaheen <adamhshaheen@gmail.com>
2026-02-08 14:17:27 -0800

    Initial sharing of project

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+++/**
+ * This is my first java app.   It prints out Hello
+ * World and my name to the console.
+ * @author Adam Shaheen, adamhshaheen@gmail.com
+ * @since 2/8/2026
+ * @version v1.0
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Adam Shaheen");
+    }
+}
